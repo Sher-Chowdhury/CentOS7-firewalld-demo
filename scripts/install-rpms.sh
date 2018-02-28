8ac11af6b5ff6bd40ad6e741b4a3e72f82310819
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-02-28 12:50:42 +0000

    added some more default rpms

--- scripts/install-rpms.sh
+++ scripts/install-rpms.sh
@@ -6,5 +6,6 @@ echo '##########################################################################
 yum install -y epel-release || exit 1
 yum install -y vim || exit 1
 yum install -y bash-completion || exit 1
+yum install -y man-pages
 yum install -y bash-completion-extras || exit 1
 yum install -y mtr || exit 1
